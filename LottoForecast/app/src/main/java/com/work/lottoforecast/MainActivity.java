f7fbcb546f696c2322fe3781dcb89fd57653cc30
Minki Kim <mkkim@email.user>
2019-09-11 00:47:06 +0900

    Start

---
+++ LottoForecast/app/src/main/java/com/work/lottoforecast/MainActivity.java
@@ -0,0 +1,15 @@
+package com.work.lottoforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*TODO 시작*/
+    }
+}
