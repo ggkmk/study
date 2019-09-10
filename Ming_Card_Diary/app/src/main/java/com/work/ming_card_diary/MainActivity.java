78e029f8474ce4a19d0924355d50288836061901
Minki Kim <mkkim@email.user>
2019-09-11 00:25:01 +0900

    Revert "main bottom monthList Layout"
    This reverts commit 4dcafec9afb4abd7eb6733de3922ad921c07be61.

--- Ming_Card_Diary/app/src/main/java/com/work/ming_card_diary/MainActivity.java
+++ Ming_Card_Diary/app/src/main/java/com/work/ming_card_diary/MainActivity.java
@@ -1,49 +1,13 @@
 package com.work.ming_card_diary;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-
-import com.work.ming_card_diary.Data.MonthData;
-import com.work.ming_card_diary.Home_Bottom_Month.Home_Bottom_MonthList_Item;
-
-import java.util.ArrayList;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    private GridLayoutManager monthListGridManager;
-    private Home_Bottom_MonthList_Item monthListItemAdapter;
-    private ArrayList<MonthData> monthListData;
-    private MonthData monthData;
-
-    @BindView(R.id.md_Diary_Month_List_Recyclerview)
-    RecyclerView diaryMonthList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-        // bottom monthList Setting
-        monthListData = new ArrayList<>();
-        for (int i = 0; i<12; i++) {
-            monthData = new MonthData();
-            String month = i+1 + "월";
-            monthData.setMonth(month);
-            monthListData.add(monthData);
-        }
-
-        monthListGridManager = new GridLayoutManager(getApplicationContext(), 1, LinearLayoutManager.HORIZONTAL, false);
-        monthListItemAdapter = new Home_Bottom_MonthList_Item(monthListData);
-        diaryMonthList.setLayoutManager(monthListGridManager);
-        diaryMonthList.setAdapter(monthListItemAdapter);
     }
 }
